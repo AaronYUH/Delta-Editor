70a37d4ac6d0146eb2c3abd9e9cf7321262ca4f0
Aaron R <aaron.akr@yahoo.com>
2024-03-16 13:08:19 -0500

    Save File & Open File testing

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <windows.h>
+#include <commdlg.h>
 
 #define WINDOW_W 500
 #define WINDOW_H 500
 #define SAVE_FILE 0
 #define PREF_PLACEHOLDER 1
 #define CLEAR_FILE 2
+#define OPEN_FILE 3
 
 // window procedure
 LRESULT CALLBACK xdProcedure(HWND, UINT, WPARAM, LPARAM);
@@ -17,8 +19,8 @@ void AddMenus(HWND);
 void AddControls(HWND);
 
 // file stuff (yuck)
-void OpenFile();
-void SaveFile();
+void openF(HWND); // save for later very issue
+void saveF();
 
 HMENU hMenu;
 HWND hEdit;
@@ -64,6 +66,9 @@ LRESULT CALLBACK xdProcedure(HWND hWnd, UINT msg, WPARAM wp, LPARAM lp) {
                     GetWindowText(hEdit, text, sizeof(text));
                     printf(text);
                     break;
+                case OPEN_FILE:
+                    openF(hWnd);
+                    break;
             }   
             break;
         case WM_CREATE:
@@ -89,6 +94,7 @@ void AddMenus(HWND hWnd) {
     // preference menu
     {
         AppendMenu(hFileMenu, MF_STRING, SAVE_FILE, "Save");
+        AppendMenu(hFileMenu, MF_STRING, OPEN_FILE, "Open");
         AppendMenu(hFileMenu, MF_STRING, CLEAR_FILE, "Clear");
     }
 
@@ -100,10 +106,10 @@ void AddControls(HWND hWnd) {
     hEdit = CreateWindowW(L"Edit", L"", WS_VISIBLE | WS_CHILD | ES_AUTOVSCROLL | ES_MULTILINE, 0, 0, WINDOW_W, WINDOW_H, hWnd, NULL, NULL, NULL);
 }
 
-void OpenFile() {
-
+void openF(HWND hWnd) {
+    return;
 }
 
-void SaveFile() {
+void saveF() {
 
 }
